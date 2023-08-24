b4c4504110c0ddbb44d79c89a7d400accde19a18
Abhay Thakur <48170773+falcon71181@users.noreply.github.com>
2023-08-24 19:11:34 +0530

    dumb&broke

---
+++ dnb.js
@@ -0,0 +1,8 @@
+//setTimeout(()=>{
+//    document.getElementById("dnb").innerHTML="Dumb & Broke"
+//},3000)
+
+function delaytext(){
+    document.getElementById("dnb").innerHTML="Dumb & Broke";
+}
+setTimeout(delaytext,3000)
